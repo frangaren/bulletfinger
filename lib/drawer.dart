1e2bf956c3228adedf0f8f4314570a5a38960d52
Francisco García <frangaren@usal.es>
2018-05-19 21:02:51 +0200

    Add drawer entries

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -6,6 +6,35 @@ class MyDrawer extends StatelessWidget {
 
   final String selectedOption;
 
+  Widget build_entry(BuildContext context, {String route, String name,
+    IconData icon}) {
+    if (this.selectedOption == route) {
+      return new ListTile(
+        leading: new Icon(
+            icon,
+            color: Color(0xFF1775D1)
+        ),
+        title: new Text(
+          name,
+          style: new TextStyle(
+            color: Color(0xFF1775D1),
+          ),
+        ),
+        onTap: () {
+          Navigator.pushNamed(context, route);
+        },
+      );
+    } else {
+      return new ListTile(
+        leading: new Icon(icon),
+        title: new Text(name),
+        onTap: () {
+          Navigator.pushNamed(context, route);
+        },
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Drawer(
@@ -23,6 +52,36 @@ class MyDrawer extends StatelessWidget {
                 ),
               ),
             ),
+          ),
+          build_entry(
+              context,
+              route: '/home',
+              name: 'Principal',
+              icon: Icons.home
+          ),
+          build_entry(
+              context,
+              route: '/games',
+              name: 'Partidas',
+              icon: Icons.games
+          ),
+          build_entry(
+              context,
+              route: '/gallery',
+              name: 'Galería',
+              icon: Icons.video_library
+          ),
+          build_entry(
+              context,
+              route: '/friends',
+              name: 'Amigos',
+              icon: Icons.contacts
+          ),
+          build_entry(
+              context,
+              route: '/statistics',
+              name: 'Estadísticas',
+              icon: Icons.show_chart
           ),
         ],
       ),
