be47524801e30e81f1d3c3359887ba6b2416ccf9
Francisco García <frangaren@usal.es>
2018-05-19 19:58:47 +0200

    Add detecting delay

--- lib/detect-peripherals-screen.dart
+++ lib/detect-peripherals-screen.dart
@@ -3,15 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class _DetectPeripheralsScreenState extends State<DetectPeripheralsScreen> {
-  RaisedButton button = new RaisedButton(
-    child: new Text('DETECTANDO'),
-    disabledTextColor: Colors.black,
-    onPressed: null,
-  );
+  bool detecting = true;
 
   @override
   void initState() {
     super.initState();
+    new Future.delayed(new Duration(seconds: 2)).then(
+        (_) => setState(() => this.detecting = false)
+    );
   }
 
   @override
@@ -20,12 +19,30 @@ class _DetectPeripheralsScreenState extends State<DetectPeripheralsScreen> {
     super.deactivate();
   }
 
+  void navigate_to_home(BuildContext context) {
+    Navigator.pushReplacementNamed(context, '/home');
+  }
+
+  RaisedButton buildButton(BuildContext context) {
+    String label = 'DETECTANDO';
+    Function callback;
+    if (!this.detecting) {
+      label = 'CONTINUAR';
+      callback = () => navigate_to_home(context);
+    }
+    return new RaisedButton(
+      child: new Text(label),
+      disabledTextColor: Colors.black,
+      onPressed: callback,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       backgroundColor: Color(0xFF1775D1),
       body: new Center(
-        child: this.button,
+        child: this.buildButton(context),
       ),
     );
   }
