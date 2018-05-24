f7b735eb0802c1e37a4fc8c8191030093d054775
Francisco García <frangaren@usal.es>
2018-05-24 21:11:08 +0200

    Add gallery grid

--- lib/views/gallery-screen.dart
+++ lib/views/gallery-screen.dart
@@ -19,7 +19,17 @@ class Gallery extends StatelessWidget {
         // the App.build method, and use it to set our appbar title.
         title: new Text('Galería'),
       ),
-      body: new Text('Test'),
+      body: new GridView.count(
+        crossAxisCount: 2,
+        children: new List.generate(100, (index) {
+          return new Center(
+            child: new Text(
+              'Item $index',
+              style: Theme.of(context).textTheme.headline,
+            ),
+          );
+        }),
+      ),
     );
   }
 
