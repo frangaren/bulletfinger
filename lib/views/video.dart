221d06b0de3aeafb48d48a0202f826d6154d5775
Francisco García <frangaren@usal.es>
2018-05-24 22:14:56 +0200

    Add video component icons

--- lib/views/video.dart
+++ lib/views/video.dart
@@ -3,8 +3,11 @@ import 'package:bulletfinger/models/video.dart' as model;
 
 class Video extends StatelessWidget {
   model.Video video;
+  Function delete;
+  Function share;
+  Function play;
 
-  Video(this.video);
+  Video(this.video, {this.delete, this.share, this.play});
 
   @override
   Widget build(BuildContext context) {
@@ -13,9 +16,72 @@ class Video extends StatelessWidget {
       child: new Card(
         child: new AspectRatio(
           aspectRatio: 1.0,
-          child: new Text(
-            video.name,
-            style: Theme.of(context).textTheme.headline,
+          child: new Container(
+            child: new Column(
+              children: <Widget>[
+                new Expanded(
+                  child: new Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      new Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          new GestureDetector(
+                            child: new Icon(
+                                Icons.delete,
+                                color: Colors.white,
+                                size: 32.0
+                            ),
+                            onTap: () {
+                              if (delete != null) {
+                                delete(this.video);
+                              }
+                            },
+                          ),
+                        ],
+                      ),
+                      new GestureDetector(
+                        child: new Icon(
+                            Icons.play_arrow,
+                            color: Colors.white,
+                            size: 64.0
+                        ),
+                        onTap: () {
+                          if (play != null) {
+                            play(this.video);
+                          }
+                        },
+                      ),
+                      new Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          new GestureDetector(
+                            child: new Icon(
+                                Icons.share,
+                                color: Colors.white,
+                                size: 32.0
+                            ),
+                            onTap: () {
+                              if (share != null) {
+                                share(this.video);
+                              }
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            decoration: new BoxDecoration(
+              image: new DecorationImage(
+                fit: BoxFit.fitHeight,
+                alignment: new Alignment(0.0, 0.0),
+                image: video.image
+              )
+            ),
           ),
         ),
       ),
