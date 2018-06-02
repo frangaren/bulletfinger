6638a5b65a30118a7b17b69ccba3258f538a5ad4
Francisco García <frangaren@usal.es>
2018-06-02 14:41:06 +0200

    Add news to home screen

--- lib/views/home-screen.dart
+++ lib/views/home-screen.dart
@@ -17,7 +17,71 @@ class HomeScreen extends StatelessWidget{
       appBar: new AppBar(
         title: new Text('Principal'),
       ),
-      body: new Text('Hello world'),
+      body: new Column(
+        children: <Widget>[
+          new Card(
+            child: new Column(
+              children: <Widget>[
+                new AspectRatio(
+                    aspectRatio: 1.7778,
+                  child: new Container(
+                    child: new Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        new Padding(
+                          padding: new EdgeInsets.all(8.0),
+                          child: new Text(
+                            'BulletFinger en Android',
+                            style: new TextStyle(
+                              color: Colors.white,
+                              fontSize: 32.0,
+                              fontWeight: FontWeight.w300,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    decoration: new BoxDecoration(
+                      image: new DecorationImage(
+                          image: new AssetImage('assets/paintball.jpg'),
+                      ),
+                    ),
+                  ),
+                ),
+                new Padding(
+                  padding: new EdgeInsets.all(8.0),
+                  child: new Text(
+                    'BulletFinger ya está disponible en la PlayStore. ¡Avisa a tus amigos y empieza una partida!',
+                    style: new TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.black87,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ),
+                new Padding(
+                  padding: new EdgeInsets.only(left:8.0, right:8.0, bottom: 8.0),
+                  child: new Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      new FlatButton(
+                          onPressed: (){},
+                          child: new Text(
+                            'VER MÁS',
+                            style: new TextStyle(
+                              color: Theme.of(context).accentColor,
+                            ),
+                          ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      )
     );
   }
 
